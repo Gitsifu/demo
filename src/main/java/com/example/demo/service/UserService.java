4293256f3f3d972a50a9ccdfa72d9a9e38766ba5
sifu <2587107273@qq.com>
2018-11-26 11:46:18 +0800

    去除无用的import

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,9 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.dao.UserMapper;
 import com.example.demo.pojo.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 
 /**
  * Created by sifu on 2017/8/13
